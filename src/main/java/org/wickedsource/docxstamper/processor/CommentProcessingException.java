154ceea05be348b4d7ed9bd7fed2b1f14bc5ef7d
Joseph Verron <3975735+josephverron@users.noreply.github.com>
2023-02-08 22:20:05 +0100

    remove unused code

--- src/main/java/org/wickedsource/docxstamper/processor/CommentProcessingException.java
+++ src/main/java/org/wickedsource/docxstamper/processor/CommentProcessingException.java
@@ -1,28 +1,15 @@
 package org.wickedsource.docxstamper.processor;
 
 import org.docx4j.wml.P;
-import org.docx4j.TextUtils;
-import org.docx4j.wml.P;
 import org.wickedsource.docxstamper.api.DocxStamperException;
 
+import static java.lang.String.format;
 import static org.docx4j.TextUtils.getText;
 
-import static java.text.MessageFormat.format;
-
 public class CommentProcessingException extends DocxStamperException {
-    private static final String PATTERN = "{0}\n" +
-            "Coordinates of offending commented paragraph within the document: \n" +
-            "{1}";
 
     public CommentProcessingException(String message, P paragraph) {
-        super(message + "\nCoordinates of offending commented paragraph within the document: \n" + getText(paragraph));
+        super(format("%s : %s", message, getText(paragraph)));
     }
 
-    private CommentProcessingException(String message, String paragraphContent) {
-        this(format(PATTERN, message, paragraphContent));
-    }
-
-    private CommentProcessingException(String message) {
-        super(message);
-    }
 }
