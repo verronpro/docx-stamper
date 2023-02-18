c00959adf7e0fa7c780ec488aa41b11f00e4404e
Joseph Verron <3975735+josephverron@users.noreply.github.com>
2023-02-18 15:30:26 +0100

    move to junit 5

--- src/test/java/org/wickedsource/docxstamper/MultiStampTest.java
+++ src/test/java/org/wickedsource/docxstamper/MultiStampTest.java
@@ -6,8 +6,7 @@ import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.wml.Tbl;
 import org.docx4j.wml.Tc;
 import org.docx4j.wml.Tr;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.wickedsource.docxstamper.context.NameContext;
 import org.wickedsource.docxstamper.util.DocumentUtil;
 
@@ -17,67 +16,71 @@ import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class MultiStampTest extends AbstractDocx4jTest {
 
-    @Test
-    public void expressionsAreResolvedOnMultiStamp() throws Docx4JException, IOException {
-        DocxStamper<NamesContext> stamper = new DocxStamper<>(new DocxStamperConfiguration().setFailOnUnresolvedExpression(false));
-        NamesContext context = new NamesContext();
+	@Test
+	public void expressionsAreResolvedOnMultiStamp() throws Docx4JException, IOException {
+		DocxStamper<NamesContext> stamper = new DocxStamper<>(new DocxStamperConfiguration().setFailOnUnresolvedExpression(
+				false));
+		NamesContext context = new NamesContext();
 
-        InputStream template = getClass().getResourceAsStream("MultiStampTest.docx");
-        OutputStream out = getOutputStream();
-        stamper.stamp(template, context, out);
-        InputStream in = getInputStream(out);
-        WordprocessingMLPackage document = WordprocessingMLPackage.load(in);
-        assertTableRows(document);
+		InputStream template = getClass().getResourceAsStream("MultiStampTest.docx");
+		OutputStream out = getOutputStream();
+		stamper.stamp(template, context, out);
+		InputStream in = getInputStream(out);
+		WordprocessingMLPackage document = WordprocessingMLPackage.load(in);
+		assertTableRows(document);
 
-        template = getClass().getResourceAsStream("MultiStampTest.docx");
-        out = getOutputStream();
-        stamper.stamp(template, context, out);
-        in = getInputStream(out);
-        document = WordprocessingMLPackage.load(in);
-        assertTableRows(document);
-    }
-
-	private static void assertRowContainsText(Tr row, String text) {
-		final List<Tc> cell0 = DocumentUtil.extractElements(row, Tc.class);
-		String cellContent = TextUtils.getText(cell0.get(0));
-		String message = String.format("'%s' is not contained in '%s'", text, cellContent);
-		Assert.assertTrue(message, cellContent.contains(text));
+		template = getClass().getResourceAsStream("MultiStampTest.docx");
+		out = getOutputStream();
+		stamper.stamp(template, context, out);
+		in = getInputStream(out);
+		document = WordprocessingMLPackage.load(in);
+		assertTableRows(document);
 	}
 
-    private void assertTableRows(WordprocessingMLPackage document) {
+	private void assertTableRows(WordprocessingMLPackage document) {
 		final List<Tbl> tablesFromObject = DocumentUtil.extractElements(document, Tbl.class);
-		Assert.assertEquals(1, tablesFromObject.size());
+		assertEquals(1, tablesFromObject.size());
 
 		final List<Tr> tableRows = DocumentUtil.extractElements(tablesFromObject.get(0), Tr.class);
-		Assert.assertEquals(5, tableRows.size());
+		assertEquals(5, tableRows.size());
 
 		assertRowContainsText(tableRows.get(0), "Homer");
 		assertRowContainsText(tableRows.get(1), "Marge");
 		assertRowContainsText(tableRows.get(2), "Bart");
 		assertRowContainsText(tableRows.get(3), "Lisa");
 		assertRowContainsText(tableRows.get(4), "Maggie");
-    }
+	}
 
-    public static class NamesContext {
+	private static void assertRowContainsText(Tr row, String text) {
+		final List<Tc> cell0 = DocumentUtil.extractElements(row, Tc.class);
+		String cellContent = TextUtils.getText(cell0.get(0));
+		String message = String.format("'%s' is not contained in '%s'", text, cellContent);
+		assertTrue(cellContent.contains(text), message);
+	}
+
+	public static class NamesContext {
 		private List<NameContext> names = new ArrayList<>();
 
-        public NamesContext() {
+		public NamesContext() {
 			this.names.add(new NameContext("Homer"));
 			this.names.add(new NameContext("Marge"));
 			this.names.add(new NameContext("Bart"));
 			this.names.add(new NameContext("Lisa"));
 			this.names.add(new NameContext("Maggie"));
-        }
+		}
 
 		public List<NameContext> getNames() {
-            return names;
-        }
+			return names;
+		}
 
 		public void setNames(List<NameContext> names) {
-            this.names = names;
-        }
-    }
+			this.names = names;
+		}
+	}
 
 }
