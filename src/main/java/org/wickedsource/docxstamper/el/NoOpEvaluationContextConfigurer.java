99e30da701dd7ed0a660fed87a197ea2678ba350
Joseph Verron <joseph@verron.pro>
2024-02-26 16:32:12 +0100

    Update NoOpEvaluationContextConfigurer class documentation
    This commit adds further explanation to the NoOpEvaluationContextConfigurer class. It expands on its role as a configuration placeholder, its potential security risks, and guidelines for using it effectively. This updated documentation will offer better clarity and insights to the users.

--- src/main/java/org/wickedsource/docxstamper/el/NoOpEvaluationContextConfigurer.java
+++ src/main/java/org/wickedsource/docxstamper/el/NoOpEvaluationContextConfigurer.java
@@ -4,14 +4,28 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.wickedsource.docxstamper.api.EvaluationContextConfigurer;
 
 /**
- * {@link org.wickedsource.docxstamper.api.EvaluationContextConfigurer} that does no customization.
- *
+ * {@link EvaluationContextConfigurer} that does no customization.
+ * <p>
+ * The NoOpEvaluationContextConfigurer is a configuration placeholder used to indicate the
+ * intention to keep the standard powerful features provided by the
+ * Spring framework's StandardEvaluationContext class.
+ * <p>
+ * StandardEvaluationContext is a powerful class by default, which can lead to potential security risks
+ * if not properly managed. This might include potential dangerous injections in the template.
+ * <p>
+ * This configurer does nothing to the StandardEvaluationContext class, and therefore all the
+ * unfiltered features are accessible. It should be used when there is a need to use the
+ * powerful features of the aforementioned class, and there is a trust that the template won't
+ * contain any dangerous injections.
+
  * @author Joseph Verron
  * @version ${version}
  */
 public class NoOpEvaluationContextConfigurer implements EvaluationContextConfigurer {
     /**
-     * {@inheritDoc}
+     * Configures the provided StandardEvaluationContext.
+     *
+     * @param context the StandardEvaluationContext to be configured, not null
      */
     @Override
     public void configureEvaluationContext(StandardEvaluationContext context) {
