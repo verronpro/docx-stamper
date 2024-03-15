e897b484e360a7b74a1e522f28c0bc3d01501dfb
Joseph Verron <joseph@verron.pro>
2024-03-15 01:31:06 +0100

    Update module dependencies and open/export statements
    Updated the module dependencies in both the main and the test module-info.java files. Additionally, reorganized the open and export statements in these modules. The changes optimize the dependency hierarchy and improve the structure of module accessibility.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,4 @@
 module pro.verron.opcstamper {
-    exports pro.verron.docxstamper.api;
-    exports pro.verron.docxstamper.preset;
-
-    exports org.wickedsource.docxstamper;
-    exports org.wickedsource.docxstamper.el;
-    exports org.wickedsource.docxstamper.processor.table;
-    exports org.wickedsource.docxstamper.util;
-    exports org.wickedsource.docxstamper.api.commentprocessor;
-    // exports org.wickedsource.docxstamper.processor;
-    exports org.wickedsource.docxstamper.api;
-
     requires spring.core;
     requires spring.expression;
 
@@ -18,4 +7,20 @@ module pro.verron.opcstamper {
     requires static org.apache.commons.io;
     requires static org.slf4j;
     requires static jakarta.xml.bind;
+
+    opens pro.verron.docxstamper.api;
+    opens pro.verron.docxstamper.preset;
+
+    exports pro.verron.docxstamper.api;
+    exports pro.verron.docxstamper.preset;
+
+    exports org.wickedsource.docxstamper;
+    exports org.wickedsource.docxstamper.api;
+    exports org.wickedsource.docxstamper.api.commentprocessor;
+    exports org.wickedsource.docxstamper.el;
+    exports org.wickedsource.docxstamper.util;
+    // exports org.wickedsource.docxstamper.processor;
+    exports org.wickedsource.docxstamper.processor.table;
+
+
 }
--- src/test/java/module-info.java
+++ src/test/java/module-info.java
@@ -1,23 +1,25 @@
 module pro.verron.opcstamper.test {
-    requires pro.verron.opcstamper;
+    requires transitive pro.verron.opcstamper;
+
     requires org.junit.jupiter.api;
     requires org.junit.jupiter.params;
+
+    requires spring.core;
     requires spring.context;
     requires spring.expression;
+
     requires org.docx4j.openxml_objects;
     requires org.docx4j.core;
+
     requires org.slf4j;
     requires jakarta.xml.bind;
-    requires spring.core;
 
-    opens pro.verron.docxstamper.test to
-            spring.expression, org.junit.platform.commons;
-
-    opens org.wickedsource.docxstamper.test to org.junit.platform.commons;
-    opens pro.verron.docxstamper.test.utils.context to spring.core;
-
-    exports pro.verron.docxstamper.test.utils.context to spring.expression;
-    exports pro.verron.docxstamper.test.commentProcessors to pro.verron.opcstamper;
-    exports pro.verron.docxstamper.test to pro.verron.opcstamper;
-    exports org.wickedsource.docxstamper.test to spring.expression;
+    opens pro.verron.docxstamper.test;
+    opens pro.verron.docxstamper.test.utils.context;
+    opens org.wickedsource.docxstamper.test;
+    
+    exports org.wickedsource.docxstamper.test;
+    exports pro.verron.docxstamper.test;
+    exports pro.verron.docxstamper.test.commentProcessors;
+    exports pro.verron.docxstamper.test.utils.context;
 }
