8fe53a7fca53354ec28b0cfd1076344953aa5083
Joseph Verron <joseph@verron.pro>
2024-04-28 08:32:31 +0800

    Add extensive module information to module-info.java
    A comprehensive documentation has been added to the module-info.java file for the 'pro.verron.officestamper' application. This includes declaring its dependencies, packages needed for runtime access and packages that need to be exported. A warning has also been added about certain packages opening and exporting that should be inaccessible in future versions.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,39 @@
+/**
+ * This module serves as the main module for the "pro.verron.officestamper" application.
+ * It declares the module dependencies and exports the necessary packages.
+ * <p>
+ * The module requires the following modules:
+ * - spring.core
+ * - spring.expression
+ * - org.docx4j.core
+ * <p>
+ * It also requires the following modules statically:
+ * - org.apache.commons.io
+ * - org.slf4j
+ * - jakarta.xml.bind
+ * <p>
+ * The module opens the following packages for reflection and runtime access:
+ * - pro.verron.docxstamper.api
+ * - pro.verron.docxstamper.preset
+ * <p>
+ * The module exports the following packages for use by other modules:
+ * - pro.verron.docxstamper.api
+ * - pro.verron.docxstamper.preset
+ * <p>
+ * Additionally, it opens the "pro.verron.docxstamper.core" package to the "pro.verron.officestamper.test" module,
+ * and exports it for use by the same module.
+ * <p>
+ * WARNING: The module also opens and exports packages that should be innacessible in the next version.
+ * These packages are:
+ * - pro.verron.docxstamper
+ * - org.wickedsource.docxstamper
+ * - org.wickedsource.docxstamper.api
+ * - org.wickedsource.docxstamper.api.commentprocessor
+ * - org.wickedsource.docxstamper.el
+ * - org.wickedsource.docxstamper.util
+ * - org.wickedsource.docxstamper.processor
+ * - org.wickedsource.docxstamper.processor.table
+ */
 module pro.verron.officestamper {
     requires spring.core;
     requires spring.expression;
@@ -16,9 +52,8 @@ module pro.verron.officestamper {
 
     opens pro.verron.docxstamper.core to pro.verron.officestamper.test;
     exports pro.verron.docxstamper.core to pro.verron.officestamper.test;
-    /**
-     * TODO: remove all the following exports in next version
-     */
+
+    // TODO_LATER: remove all the following exports in next version
     opens pro.verron.docxstamper;
     exports pro.verron.docxstamper;
     exports org.wickedsource.docxstamper;
