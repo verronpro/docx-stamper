f90fd1bcbf9a4a083f2445c1daf265996a2d3471
Joseph Verron <joseph@verron.pro>
2024-04-30 09:04:05 +0800

    Change superclass of UnresolvedExpressionException
    Updated the superclass of UnresolvedExpressionException from DocxStamperException to OfficeStamperException. This update was necessary to align with the new exception handling strategy.

--- src/main/java/org/wickedsource/docxstamper/api/UnresolvedExpressionException.java
+++ src/main/java/org/wickedsource/docxstamper/api/UnresolvedExpressionException.java
@@ -1,5 +1,7 @@
 package org.wickedsource.docxstamper.api;
 
+import pro.verron.docxstamper.api.OfficeStamperException;
+
 /**
  * This exception is thrown if an expression could not be processed by any comment processor.
  *
@@ -8,7 +10,8 @@ package org.wickedsource.docxstamper.api;
  * @version ${version}
  * @since 1.0.3
  */
-public class UnresolvedExpressionException extends DocxStamperException {
+public class UnresolvedExpressionException
+        extends OfficeStamperException {
     /**
      * <p>Constructor for UnresolvedExpressionException.</p>
      *
@@ -16,6 +19,7 @@ public class UnresolvedExpressionException extends DocxStamperException {
      * @param cause      the root cause for this exception
      */
     public UnresolvedExpressionException(String expression, Throwable cause) {
-        super(String.format("The following expression could not be processed by any comment processor: %s", expression), cause);
+        super(String.format("The following expression could not be processed by any comment processor: %s", expression),
+                cause);
     }
 }
