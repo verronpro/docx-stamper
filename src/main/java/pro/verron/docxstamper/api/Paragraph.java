c927807231e822781f9171b698532e01f6ca4043
Joseph Verron <joseph@verron.pro>
2024-04-30 09:09:44 +0800

    Remove generic type from Paragraph interface
    The generic type <T> was removed from the Paragraph interface to make it more flexible. Consequently, the replace method now accepts an Object as the replacement parameter, instead of a specific type T.

--- src/main/java/pro/verron/docxstamper/api/Paragraph.java
+++ src/main/java/pro/verron/docxstamper/api/Paragraph.java
@@ -4,7 +4,7 @@ package pro.verron.docxstamper.api;
  * The Paragraph interface represents a paragraph in a text document.
  * It provides methods for replacing a placeholder within the paragraph and retrieving the paragraph as a string.
  */
-public interface Paragraph<T> {
+public interface Paragraph {
 
     /**
      * Replaces a placeholder in the given paragraph with the specified replacement.
@@ -12,7 +12,7 @@ public interface Paragraph<T> {
      * @param placeholder The placeholder to be replaced.
      * @param replacement The replacement for the placeholder.
      */
-    void replace(Placeholder placeholder, T replacement);
+    void replace(Placeholder placeholder, Object replacement);
 
     /**
      * Returns the paragraph as a string.
