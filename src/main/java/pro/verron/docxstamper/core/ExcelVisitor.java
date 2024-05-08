519fc56b0afe9610eccb68a6b5e16ab8200920f5
Joseph Verron <joseph@verron.pro>
2024-05-08 22:56:39 +0800

    Refactor PowerpointVisitor and ExcelVisitor classes for better logging and error handling
    Updated the PowerpointVisitor and ExcelVisitor classes to include better error handling and logging features. Certain methods in these classes now have additional code to log ignored instances and handle unexpected visits. Also, refactored the structure of some methods to improve code readability and maintainability.

--- src/main/java/pro/verron/docxstamper/core/ExcelVisitor.java
+++ src/main/java/pro/verron/docxstamper/core/ExcelVisitor.java
@@ -11,16 +11,38 @@ import org.docx4j.openpackaging.parts.SpreadsheetML.Styles;
 import org.docx4j.openpackaging.parts.SpreadsheetML.WorkbookPart;
 import org.docx4j.openpackaging.parts.SpreadsheetML.WorksheetPart;
 import org.docx4j.openpackaging.parts.ThemePart;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
 import org.xlsx4j.sml.*;
 import pro.verron.docxstamper.api.OfficeStamperException;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 abstract class ExcelVisitor {
 
-    public final void visit(Object object) {
+    private static final Logger logger = LoggerFactory.getLogger(ExcelVisitor.class);
+
+    private static void unexpectedVisit(Object object) {
+        var env = System.getenv();
+        var throwOnUnexpectedVisit = Boolean.parseBoolean(env.getOrDefault("throw-on-unexpected-visit", "false"));
+        var message = "Unknown case : %s %s".formatted(object, object.getClass());
+        if (throwOnUnexpectedVisit)
+            throw new OfficeStamperException(message);
+        else
+            logger.debug(message);
+    }
+
+    private static void ignore(@Nullable Object ignored1) {
+        logger.trace("ignored visit of '{}' object", ignored1);
+    }
+
+    public final void visit(@Nullable Object object) {
+
         before(object);
         try {
             if (object instanceof SpreadsheetMLPackage element) visit(element.getParts());
@@ -28,11 +50,11 @@ abstract class ExcelVisitor {
             else if (object instanceof Parts element) visit(element.getParts());
             else if (object instanceof WorksheetPart element) visit(element.getContents());
             else if (object instanceof WorkbookPart element) visit(element.getContents());
-            else if (object instanceof DocPropsCorePart ignored) { /* do nothing */ }
-            else if (object instanceof DocPropsExtendedPart ignored) { /* do nothing */ }
-            else if (object instanceof Styles ignored) { /* do nothing */ }
-            else if (object instanceof SharedStrings element) {visit(element.getContents());}
-            else if (object instanceof ThemePart ignored) { /* do nothing */ }
+            else if (object instanceof DocPropsCorePart ignored) ignore(ignored);
+            else if (object instanceof DocPropsExtendedPart ignored) ignore(ignored);
+            else if (object instanceof Styles ignored) ignore(ignored);
+            else if (object instanceof SharedStrings element) visit(element.getContents());
+            else if (object instanceof ThemePart ignored) ignore(ignored);
 
             else if (object instanceof Workbook element) visit(element.getSheets());
             else if (object instanceof Sheets element) visit(element.getSheet());
@@ -43,22 +65,26 @@ abstract class ExcelVisitor {
             else if (object instanceof CTRst element) visit(element.getR());
             else if (object instanceof CTSst element) visit(element.getSi());
             else if (object instanceof CTRElt element) visit(element.getT());
-            else if (object instanceof CTXstringWhitespace ignored) { /* do nothing */ }
+            else if (object instanceof CTXstringWhitespace ignored) ignore(ignored);
             else if (object instanceof JAXBElement<?> element) visit(element.getValue());
             else if (object instanceof Sheet element) visit(element.getState());
-            else if (object instanceof STSheetState ignored) { /* do nothing */ }
+            else if (object instanceof STSheetState ignored) ignore(ignored);
 
             else if (object instanceof List<?> element) element.forEach(this::visit);
             else if (object instanceof Set<?> element) element.forEach(this::visit);
             else if (object instanceof Map<?, ?> element) visit(element.entrySet());
-            else if (object instanceof Map.Entry<?, ?> element) visit(element.getValue());
-            else if (object == null) { /* do nothing */ }
-            // TODO: replace this test throws by a noop before usage in the wild
-            else throw new OfficeStamperException("Unknown case : %s %s".formatted(object, object.getClass()));
+            else if (object instanceof Entry<?, ?> element) visit(element.getKey(), element.getValue());
+            else if (object == null) ignore(null);
+            else unexpectedVisit(object);
         } catch (Docx4JException e) {
             throw new OfficeStamperException(e);
         }
     }
 
-    protected abstract void before(Object object);
+    private void visit(Object... objs) {
+        Arrays.stream(objs)
+              .forEach(this::visit);
+    }
+
+    protected abstract void before(@Nullable Object object);
 }
--- src/main/java/pro/verron/docxstamper/core/PowerpointVisitor.java
+++ src/main/java/pro/verron/docxstamper/core/PowerpointVisitor.java
@@ -9,32 +9,51 @@ import org.docx4j.openpackaging.parts.*;
 import org.docx4j.openpackaging.parts.PresentationML.*;
 import org.docx4j.openpackaging.parts.WordprocessingML.ImageJpegPart;
 import org.pptx4j.pml.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.Nullable;
 import pro.verron.docxstamper.api.OfficeStamperException;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 abstract class PowerpointVisitor {
 
-    public final void visit(Object object) {
+    private static final Logger logger = LoggerFactory.getLogger(PowerpointVisitor.class);
+
+    private static void unexpectedVisit(Object object) {
+        var env = System.getenv();
+        var throwOnUnexpectedVisit = Boolean.parseBoolean(env.getOrDefault("throw-on-unexpected-visit", "false"));
+        var message = "Unknown case : %s %s".formatted(object, object.getClass());
+        if (throwOnUnexpectedVisit)
+            throw new OfficeStamperException(message);
+        else
+            logger.debug(message);
+    }
+
+    private static void ignore(@Nullable Object ignored1) {
+        logger.trace("ignored visit of '{}' object", ignored1);
+    }
+
+    public final void visit(@Nullable Object object) {
         before(object);
         try {
             if (object instanceof PresentationMLPackage element) visit(element.getParts());
-            else if (object instanceof PartName ignored) { /* Do nothing */ }
+            else if (object instanceof PartName ignored) ignore(ignored);
             else if (object instanceof Parts element) visit(element.getParts());
-            else if (object instanceof SlideLayoutPart ignored) { /* Do nothing */ }
-            else if (object instanceof ImageJpegPart ignored) { /* Do nothing */ }
-            else if (object instanceof ThemePart ignored) { /* Do nothing */ }
-            else if (object instanceof DocPropsCorePart ignored) { /* Do nothing */ }
-            else if (object instanceof DocPropsExtendedPart ignored) { /* Do nothing */ }
-            else if (object instanceof SlideMasterPart ignored) { /* Do nothing */ }
-            else if (object instanceof ViewPropertiesPart ignored) { /* Do nothing */ }
-            else if (object instanceof PresentationPropertiesPart ignored) { /* Do nothing */ }
-            else if (object instanceof TableStylesPart ignored) { /* Do nothing */ }
+            else if (object instanceof SlideLayoutPart ignored) ignore(ignored);
+            else if (object instanceof ImageJpegPart ignored) ignore(ignored);
+            else if (object instanceof ThemePart ignored) ignore(ignored);
+            else if (object instanceof DocPropsCorePart ignored) ignore(ignored);
+            else if (object instanceof DocPropsExtendedPart ignored) ignore(ignored);
+            else if (object instanceof SlideMasterPart ignored) ignore(ignored);
+            else if (object instanceof ViewPropertiesPart ignored) ignore(ignored);
+            else if (object instanceof PresentationPropertiesPart ignored) ignore(ignored);
+            else if (object instanceof TableStylesPart ignored) ignore(ignored);
             else if (object instanceof MainPresentationPart element) visit(element.getContents());
 
-            else if (object instanceof List<?> elements) elements.forEach(this::visit);
-            else if (object instanceof Map<?, ?> elements) elements.forEach(this::visit);
             else if (object instanceof SlidePart element) visit(element.getContents());
             else if (object instanceof Sld element) visit(element.getCSld());
             else if (object instanceof CommonSlideData element) visit(element.getSpTree());
@@ -42,22 +61,27 @@ abstract class PowerpointVisitor {
             else if (object instanceof Shape element) visit(element.getTxBody());
             else if (object instanceof CTTextBody element) visit(element.getP());
             else if (object instanceof CTTextParagraph element) visit(element.getEGTextRun());
-            else if (object instanceof CTRegularTextRun ignored) { /* Do nothing */ }
-            else if (object instanceof Presentation.SldSz ignored) { /* Do Nothing */ }
-            else if (object instanceof Presentation ignored) { /* Do Nothing */ }
-            else if (object == null) { /* Do Nothing */ }
-            // TODO: replace this test throws by a noop before usage in the wild
-            else throw new OfficeStamperException("Unknown case %s : %s".formatted(object.getClass(), object));
+            else if (object instanceof CTRegularTextRun ignored) ignore(ignored);
+            else if (object instanceof Presentation.SldSz ignored) ignore(ignored);
+            else if (object instanceof Presentation ignored) ignore(ignored);
+
+            else if (object instanceof List<?> element) element.forEach(this::visit);
+            else if (object instanceof Set<?> element) element.forEach(this::visit);
+            else if (object instanceof Map<?, ?> element) visit(element.entrySet());
+            else if (object instanceof Map.Entry<?, ?> element) visit(element.getKey(), element.getValue());
+
+            else if (object == null) ignore(null);
+            else unexpectedVisit(object);
         } catch (Docx4JException e) {
             throw new OfficeStamperException(e);
         }
     }
 
-    private void visit(Object o1, Object o2) {
-        visit(o1);
-        visit(o2);
+    private void visit(Object... objs) {
+        Arrays.stream(objs)
+              .forEach(this::visit);
     }
 
-    protected abstract void before(Object object);
+    protected abstract void before(@Nullable Object object);
 
 }
