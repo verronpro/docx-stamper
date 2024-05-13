a605a548329567a1e3dcfcbad62c609c19dfa8f8
Joseph Verron <joseph@verron.pro>
2024-05-13 23:42:48 +0800

    Refactor OfficeStampers to use StreamStamper
    In the OfficeStampers.java file, updated docxStamper methods to return StreamStamper instances instead of OfficeStamper instances. In addition, implemented a new method to handle WordprocessingMLPackage loading that catches and rethrows Docx4JException as OfficeStamperException.

--- src/main/java/pro/verron/officestamper/preset/OfficeStampers.java
+++ src/main/java/pro/verron/officestamper/preset/OfficeStampers.java
@@ -1,11 +1,15 @@
 package pro.verron.officestamper.preset;
 
+import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.wickedsource.docxstamper.DocxStamper;
 import org.wickedsource.docxstamper.preprocessor.MergeSameStyleRuns;
 import org.wickedsource.docxstamper.preprocessor.RemoveProofErrors;
-import pro.verron.officestamper.api.OfficeStamper;
 import pro.verron.officestamper.api.OfficeStamperConfiguration;
+import pro.verron.officestamper.api.OfficeStamperException;
+import pro.verron.officestamper.api.StreamStamper;
+
+import java.io.InputStream;
 
 /**
  * Main class of the docx-stamper library.
@@ -19,27 +23,38 @@ import pro.verron.officestamper.api.OfficeStamperConfiguration;
  */
 public class OfficeStampers {
 
-	/**
-	 * Creates a new instance of the {@link DocxStamper} class with the specified {@link OfficeStamperConfiguration}.
-	 *
-	 * @param config the configuration for the docx stamper
-	 *
-	 * @return a new instance of the {@link DocxStamper} class
-	 */
-	public static OfficeStamper<WordprocessingMLPackage> docxStamper(
-			OfficeStamperConfiguration config
-	) {
-		return new DocxStamper<>(config);
-	}
+    /**
+     * Creates a new DocxStamper with the default configuration.
+     * Also adds the {@link RemoveProofErrors} and {@link MergeSameStyleRuns} preprocessors.
+     *
+     * @return a new DocxStamper
+     */
+    public static StreamStamper<WordprocessingMLPackage> docxStamper() {
+        return docxStamper(OfficeStamperConfigurations.standardWithPreprocessing());
+    }
 
-	/**
-	 * Creates a new DocxStamper with the default configuration.
-	 * Also adds the {@link RemoveProofErrors} and {@link MergeSameStyleRuns} preprocessors.
-	 *
-	 * @return a new DocxStamper
-	 */
-	public static OfficeStamper<WordprocessingMLPackage> docxStamper() {
-		return new DocxStamper<>(OfficeStamperConfigurations.standardWithPreprocessing());
-	}
+    /**
+     * Creates a new instance of the {@link DocxStamper} class with the specified {@link OfficeStamperConfiguration}.
+     *
+     * @param config the configuration for the docx stamper
+     *
+     * @return a new instance of the {@link DocxStamper} class
+     */
+    public static StreamStamper<WordprocessingMLPackage> docxStamper(
+            OfficeStamperConfiguration config
+    ) {
+        return new StreamStamper<>(
+                OfficeStampers::loadWord,
+                new DocxStamper<Object>(config)
+        );
+    }
+
+    private static WordprocessingMLPackage loadWord(InputStream is) {
+        try {
+            return WordprocessingMLPackage.load(is);
+        } catch (Docx4JException e) {
+            throw new OfficeStamperException(e);
+        }
+    }
 
 }
