69cddecc58527371463822be7a9bfdc27a53dfc4
Joseph Verron <joseph@verron.pro>
2024-05-13 23:50:26 +0800

    Update Main.java to support legacy and new stamping methods
    The Main.java file has been updated to support both the older (legacy) and the newer methods of "stamping" files. The two methods are demonstrated separately via the legacyStampDiagnostic() and stampDiagnostic() methods. A helper method, createOutStream(), has been added for creating output streams.

--- officestamper-examples/src/main/java/pro/verron/officestamper/Main.java
+++ officestamper-examples/src/main/java/pro/verron/officestamper/Main.java
@@ -1,9 +1,14 @@
 package pro.verron.officestamper;
 
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static pro.verron.officestamper.Examples.legacyStampDiagnostic;
+import static pro.verron.officestamper.Examples.stampDiagnostic;
 
 public class Main {
 
@@ -11,9 +16,18 @@ public class Main {
 
     public static void main(String[] args)
             throws Exception {
-        var outputPath = Files.createTempFile("Diagnostic-", ".docx");
-        var outputStream = Files.newOutputStream(outputPath);
-        logger.log(Level.INFO, "Stamping to file: " + outputPath);
-        Examples.stampDiagnostic(outputStream);
+        // Use the way prior to docx-stamper 1.6.8
+        legacyStampDiagnostic(createOutStream("LegacyDiagnostic-"));
+
+        // Use the way from docx-stamper 1.6.8 and with office-stamper
+        stampDiagnostic(createOutStream("Diagnostic-"));
     }
+
+    private static OutputStream createOutStream(String prefix)
+            throws IOException {
+        var outputPath = Files.createTempFile(prefix, ".docx");
+        logger.log(Level.INFO, "Stamping to file: " + outputPath);
+        return Files.newOutputStream(outputPath);
+    }
+
 }
