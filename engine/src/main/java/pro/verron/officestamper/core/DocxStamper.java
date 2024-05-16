30a73eca9d5e3c6f1e7608e9f6029d1f2cf4ee9c
Joseph Verron <joseph@verron.pro>
2024-05-16 09:06:44 +0800

    refactor!: CommentProcessorRegistry class location and update module exports
    The CommentProcessorRegistry class has been moved from the org.wickedsource.docxstamper.processor package to the pro.verron.officestamper.core package. The relevant exports in module-info have been updated to reflect this reorganization of code for better package structure and separation of concerns. Moreover, unnecessary import statements have been removed, thus, keep the code clean and concise.

--- engine/src/main/java/pro/verron/officestamper/core/DocxStamper.java
+++ engine/src/main/java/pro/verron/officestamper/core/DocxStamper.java
@@ -8,7 +8,6 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.lang.NonNull;
 import org.wickedsource.docxstamper.el.ExpressionResolver;
 import org.wickedsource.docxstamper.el.StandardMethodResolver;
-import org.wickedsource.docxstamper.processor.CommentProcessorRegistry;
 import pro.verron.officestamper.api.*;
 
 import java.io.InputStream;
