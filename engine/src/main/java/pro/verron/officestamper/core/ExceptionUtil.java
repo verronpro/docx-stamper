77a413baab3f923ace94e28fa18a71becf5fa8a8
Joseph Verron <joseph@verron.pro>
2024-05-21 09:41:06 +0800

    feat: Refactor expression resolution and error handling
    Refactored the process of resolving expressions in ExpressionResolver and PlaceholderReplacer. Included a new method "setContext" in ExpressionResolver, which separately sets the context against which the expression will be resolved. Error handling is also streamlined with new ExceptionUtil class. Enhanced debug logging has been implemented.

---
+++ engine/src/main/java/pro/verron/officestamper/core/ExceptionUtil.java
@@ -0,0 +1,62 @@
+package pro.verron.officestamper.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.expression.ExpressionException;
+import pro.verron.officestamper.api.OfficeStamperException;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+import java.util.function.Supplier;
+
+public class ExceptionUtil {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExceptionUtil.class);
+    private static final Map<Boolean, Map<Boolean, BiConsumer<Exception, String>>> EXCEPTION_HANDLERS;
+
+    static {
+        EXCEPTION_HANDLERS = new HashMap<>();
+        var verboseHandlers = new HashMap<Boolean, BiConsumer<Exception, String>>();
+        verboseHandlers.put(true, ExceptionUtil::throwStacktrace); // TRACE ON - THROW WITH STACKTRACE
+        verboseHandlers.put(false, ExceptionUtil::logStacktrace); // TRACE ON - LOG WITH STACKTRACE
+        EXCEPTION_HANDLERS.put(true, verboseHandlers);
+        var quietHandlers = new HashMap<Boolean, BiConsumer<Exception, String>>();
+        quietHandlers.put(true, ExceptionUtil::throwSimple); // TRACE OFF - THROW W/O STACKTRACE
+        quietHandlers.put(false, ExceptionUtil::logSimple); // TRACE OFF - LOG W/O STACKTRACE
+        EXCEPTION_HANDLERS.put(false, quietHandlers);
+    }
+
+    static void treatException(ExpressionException exception, boolean shouldThrow, String msg) {
+        treatException(exception, shouldThrow, msg, () -> "Do not expect a return");
+    }
+
+    static <T> T treatException(
+            ExpressionException exception, boolean shouldThrow, String msg,
+            Supplier<T> supplier
+    ) {
+        EXCEPTION_HANDLERS
+                .get(logger.isTraceEnabled())
+                .get(shouldThrow)
+                .accept(exception, msg);
+        if (!logger.isTraceEnabled())
+            logger.info("Set log level to TRACE to view Stacktrace.");
+        return supplier.get();
+    }
+
+    private static void throwStacktrace(Exception e, String msg) {
+        throw new OfficeStamperException(msg, e);
+    }
+
+    private static void logStacktrace(Exception e, String msg) {
+        logger.warn(msg, e);
+    }
+
+    private static void throwSimple(Exception e, String msg) {
+        throw new OfficeStamperException(msg);
+    }
+
+    private static void logSimple(Exception e, String msg) {
+        logger.warn(msg);
+    }
+}
