af17962572edddb0323aff7216b448085da8495a
Joseph Verron <joseph@verron.pro>
2024-06-13 09:37:30 +0800

    fix: replace toList with toCollection method in CommentUtil
    Replace the toList method with toCollection(ArrayList::new) in the CommentUtil class. It improves the flexibility of the code by allowing the selection of the output collection type, and specifically assists in resolving a test regression.

--- engine/src/main/java/pro/verron/officestamper/core/CommentUtil.java
+++ engine/src/main/java/pro/verron/officestamper/core/CommentUtil.java
@@ -399,7 +399,7 @@ public class CommentUtil {
         // copy the elements without comment range anchors
         var finalElements = elements.stream()
                                     .map(XmlUtils::deepCopy)
-                                    .toList();
+                                    .collect(Collectors.toCollection(ArrayList::new));
         deleteCommentFromElements(comment, finalElements);
         target.getMainDocumentPart()
               .getContent()
