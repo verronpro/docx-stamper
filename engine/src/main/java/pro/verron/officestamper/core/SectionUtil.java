538e94266395018b2eec632b0319a74b3191910c
Joseph Verron <joseph@verron.pro>
2024-10-04 14:46:05 +0800

    Improve section break detection and use Optional
    Refactor methods to use Optional and enhance section break detection. Replaced null checks with Optional to improve code readability and robustness. Also added additional logging for better traceability of actions.

--- engine/src/main/java/pro/verron/officestamper/core/SectionUtil.java
+++ engine/src/main/java/pro/verron/officestamper/core/SectionUtil.java
@@ -1,15 +1,17 @@
 package pro.verron.officestamper.core;
 
 import org.docx4j.XmlUtils;
-import org.docx4j.jaxb.Context;
-import org.docx4j.wml.*;
+import org.docx4j.wml.ContentAccessor;
+import org.docx4j.wml.P;
+import org.docx4j.wml.PPr;
+import org.docx4j.wml.SectPr;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.lang.Nullable;
 import pro.verron.officestamper.api.OfficeStamperException;
 import pro.verron.officestamper.utils.WmlFactory;
 
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
 
@@ -21,68 +23,58 @@ import static java.util.Optional.ofNullable;
  * @since 1.6.2
  */
 public class SectionUtil {
-	private static final Logger log = LoggerFactory.getLogger(SectionUtil.class);
+    private static final Logger log = LoggerFactory.getLogger(SectionUtil.class);
 
     private SectionUtil() {
-		throw new OfficeStamperException("Utility class shouldn't be instantiated");
+        throw new OfficeStamperException("Utility class shouldn't be instantiated");
     }
 
-	private static final ObjectFactory factory = Context.getWmlObjectFactory();
-
-	/**
-	 * Creates a new section break object.
-	 *
-	 * @param firstObject a {@link Object} object
+    /**
+     * Creates a new section break object.
+     *
+     * @param firstObject a {@link Object} object
 	 * @param parent      a {@link ContentAccessor} object
-	 * @return a new section break object.
-	 */
-	@Nullable
-	public static SectPr getPreviousSectionBreakIfPresent(Object firstObject, ContentAccessor parent) {
-		List<Object> parentContent = parent.getContent();
-		int pIndex = parentContent.indexOf(firstObject);
+     * @return a new section break object.
+     */
+    public static Optional<SectPr> getPreviousSectionBreakIfPresent(Object firstObject, ContentAccessor parent) {
+        List<Object> parentContent = parent.getContent();
+        int pIndex = parentContent.indexOf(firstObject);
 
-		int i = pIndex - 1;
-		while (i >= 0) {
-			if (parentContent.get(i) instanceof P prevParagraph) {
-				// the first P preceding the object is the one potentially carrying a section break
-				PPr pPr = prevParagraph.getPPr();
-				if (pPr != null && pPr.getSectPr() != null) {
-					return pPr.getSectPr();
-				} else return null;
-			}
-			i--;
-		}
-		log.info("No previous section break found from : {}, first object index={}", parent, pIndex);
-		return null;
-	}
+        int i = pIndex - 1;
+        while (i >= 0) {
+            if (parentContent.get(i) instanceof P prevParagraph) {
+                // the first P preceding the object is the one potentially carrying a section break
+                return ofNullable(prevParagraph.getPPr())
+                        .map(PPr::getSectPr);
+            }
+            else log.debug("The previous sibling was not a P, continuing search");
+            i--;
+        }
+        log.info("No previous section break found from : {}, first object index={}", parent, pIndex);
+        return Optional.empty();
+    }
 
-	/**
-	 * Creates a new section break object.
-	 *
-	 * @return a new section break object.
-	 * @param p a {@link P} object
-	 */
-	@Nullable
-	public static SectPr getParagraphSectionBreak(P p) {
-		return p.getPPr() != null && p.getPPr().getSectPr() != null
-				? p.getPPr().getSectPr()
-				: null;
-	}
+    /**
+     * Creates a new section break object.
+     *
+     * @param objects a {@link List} object
+     *
+     * @return a new section break object.
+     */
+    public static boolean hasOddNumberOfSectionBreaks(List<Object> objects) {
+        return objects.stream()
+                      .filter(P.class::isInstance)
+                      .map(P.class::cast)
+                      .filter(SectionUtil::hasSectionBreak)
+                      .count() % 2 != 0;
+    }
 
-	/**
-	 * Creates a new section break object.
-	 *
-	 * @return a new section break object.
-	 * @param objects a {@link List} object
-	 */
-	public static boolean isOddNumberOfSectionBreaks(List<Object> objects) {
-		long count = objects.stream()
-				.filter(P.class::isInstance)
-				.map(P.class::cast)
-							.filter(p -> p.getPPr() != null && p.getPPr().getSectPr() != null)
-							.count();
-		return count % 2 != 0;
-	}
+    private static boolean hasSectionBreak(P p) {
+        var pPPr = p.getPPr();
+        if (pPPr == null) return false;
+        var pPPrSectPr = pPPr.getSectPr();
+        return pPPrSectPr != null;
+    }
 
     /**
      * Creates a new section break object.
