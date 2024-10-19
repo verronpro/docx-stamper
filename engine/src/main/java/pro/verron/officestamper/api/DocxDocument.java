efe5abf6aac709f785005aeaaf49c5d739ee5094
Joseph Verron <joseph@verron.pro>
2024-10-19 20:38:46 +0800

    Remove unused import from DocxDocument.java
    Deleted the unnecessary import of CommentsPart in the DocxDocument class. This cleanup helps reduce clutter and can improve the maintainability of the code.

--- engine/src/main/java/pro/verron/officestamper/api/DocxDocument.java
+++ engine/src/main/java/pro/verron/officestamper/api/DocxDocument.java
@@ -1,7 +1,6 @@
 package pro.verron.officestamper.api;
 
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
-import org.docx4j.openpackaging.parts.WordprocessingML.CommentsPart;
 
 import java.util.stream.Stream;
 
