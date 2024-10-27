e416fdccf2b880335dd1fc77e3c6f7fadf9fd26a
Joseph Verron <joseph@verron.pro>
2024-10-27 20:48:34 +0800

    Refactor visit method to use switch expression
    Replace a series of if-else statements with a more concise switch expression in the visit method. This change improves readability and maintainability by reducing the complexity of handling different object types. The update includes an additional null check for the visit method.

--- engine/src/main/java/pro/verron/officestamper/experimental/PowerpointVisitor.java
+++ engine/src/main/java/pro/verron/officestamper/experimental/PowerpointVisitor.java
@@ -25,13 +25,12 @@ abstract class PowerpointVisitor {
     private static final Logger logger = LoggerFactory.getLogger(PowerpointVisitor.class);
 
     private static void unexpectedVisit(Object object) {
+        assert object != null : "Cannot visit a null object";
         var env = System.getenv();
         var throwOnUnexpectedVisit = Boolean.parseBoolean(env.getOrDefault("throw-on-unexpected-visit", "false"));
         var message = "Unknown case : %s %s".formatted(object, object.getClass());
-        if (throwOnUnexpectedVisit)
-            throw new OfficeStamperException(message);
-        else
-            logger.debug(message);
+        if (throwOnUnexpectedVisit) throw new OfficeStamperException(message);
+        else logger.debug(message);
     }
 
     private static void ignore(@Nullable Object ignored) {
@@ -48,38 +47,37 @@ abstract class PowerpointVisitor {
     public final void visit(@Nullable Object object) {
         before(object);
         try {
-            if (object instanceof PresentationMLPackage element) visit(element.getParts());
-            else if (object instanceof PartName ignored) ignore(ignored);
-            else if (object instanceof Parts element) visit(element.getParts());
-            else if (object instanceof SlideLayoutPart ignored) ignore(ignored);
-            else if (object instanceof ImageJpegPart ignored) ignore(ignored);
-            else if (object instanceof ThemePart ignored) ignore(ignored);
-            else if (object instanceof DocPropsCorePart ignored) ignore(ignored);
-            else if (object instanceof DocPropsExtendedPart ignored) ignore(ignored);
-            else if (object instanceof SlideMasterPart ignored) ignore(ignored);
-            else if (object instanceof ViewPropertiesPart ignored) ignore(ignored);
-            else if (object instanceof PresentationPropertiesPart ignored) ignore(ignored);
-            else if (object instanceof TableStylesPart ignored) ignore(ignored);
-            else if (object instanceof MainPresentationPart element) visit(element.getContents());
-
-            else if (object instanceof SlidePart element) visit(element.getContents());
-            else if (object instanceof Sld element) visit(element.getCSld());
-            else if (object instanceof CommonSlideData element) visit(element.getSpTree());
-            else if (object instanceof GroupShape element) visit(element.getSpOrGrpSpOrGraphicFrame());
-            else if (object instanceof Shape element) visit(element.getTxBody());
-            else if (object instanceof CTTextBody element) visit(element.getP());
-            else if (object instanceof CTTextParagraph element) visit(element.getEGTextRun());
-            else if (object instanceof CTRegularTextRun ignored) ignore(ignored);
-            else if (object instanceof Presentation.SldSz ignored) ignore(ignored);
-            else if (object instanceof Presentation ignored) ignore(ignored);
-
-            else if (object instanceof List<?> element) element.forEach(this::visit);
-            else if (object instanceof Set<?> element) element.forEach(this::visit);
-            else if (object instanceof Map<?, ?> element) visit(element.entrySet());
-            else if (object instanceof Map.Entry<?, ?> element) visit(element.getKey(), element.getValue());
-
-            else if (object == null) ignore(null);
-            else unexpectedVisit(object);
+            switch (object) {
+                case PresentationMLPackage element -> visit(element.getParts());
+                case PartName ignored -> ignore(ignored);
+                case Parts element -> visit(element.getParts());
+                case SlideLayoutPart ignored -> ignore(ignored);
+                case ImageJpegPart ignored -> ignore(ignored);
+                case ThemePart ignored -> ignore(ignored);
+                case DocPropsCorePart ignored -> ignore(ignored);
+                case DocPropsExtendedPart ignored -> ignore(ignored);
+                case SlideMasterPart ignored -> ignore(ignored);
+                case ViewPropertiesPart ignored -> ignore(ignored);
+                case PresentationPropertiesPart ignored -> ignore(ignored);
+                case TableStylesPart ignored -> ignore(ignored);
+                case MainPresentationPart element -> visit(element.getContents());
+                case SlidePart element -> visit(element.getContents());
+                case Sld element -> visit(element.getCSld());
+                case CommonSlideData element -> visit(element.getSpTree());
+                case GroupShape element -> visit(element.getSpOrGrpSpOrGraphicFrame());
+                case Shape element -> visit(element.getTxBody());
+                case CTTextBody element -> visit(element.getP());
+                case CTTextParagraph element -> visit(element.getEGTextRun());
+                case CTRegularTextRun ignored -> ignore(ignored);
+                case Presentation.SldSz ignored -> ignore(ignored);
+                case Presentation ignored -> ignore(ignored);
+                case List<?> element -> element.forEach(this::visit);
+                case Set<?> element -> element.forEach(this::visit);
+                case Map<?, ?> element -> visit(element.entrySet());
+                case Map.Entry<?, ?> element -> visit(element.getKey(), element.getValue());
+                case null -> ignore(null);
+                default -> unexpectedVisit(object);
+            }
         } catch (Docx4JException e) {
             throw new OfficeStamperException(e);
         }
