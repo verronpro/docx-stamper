171af090765b3fdd0a6332dd4b7fda2dc1976400
Joseph Verron <joseph@verron.pro>
2024-10-27 18:49:35 +0800

    Format DocxImageExtractor for consistency
    Refactor formatting in `DocxImageExtractor.java` for better readability and consistency. Adjust indents and line breaks, and re-order methods logically. No functional changes applied.

--- engine/src/main/java/pro/verron/officestamper/core/DocxImageExtractor.java
+++ engine/src/main/java/pro/verron/officestamper/core/DocxImageExtractor.java
@@ -23,50 +23,87 @@ import java.util.Arrays;
  */
 public class DocxImageExtractor {
 
-	private final WordprocessingMLPackage wordprocessingMLPackage;
+    private final WordprocessingMLPackage wordprocessingMLPackage;
 
-	/**
-	 * Creates a new image extractor for the given docx document.
-	 *
-	 * @param wordprocessingMLPackage the docx document to extract images from.
-	 */
-	public DocxImageExtractor(WordprocessingMLPackage wordprocessingMLPackage) {
-		this.wordprocessingMLPackage = wordprocessingMLPackage;
-	}
+    /**
+     * Creates a new image extractor for the given docx document.
+     *
+     * @param wordprocessingMLPackage the docx document to extract images from.
+     */
+    public DocxImageExtractor(WordprocessingMLPackage wordprocessingMLPackage) {
+        this.wordprocessingMLPackage = wordprocessingMLPackage;
+    }
 
-	/**
-	 * Extract an image bytes from an embedded image run.
-	 *
-	 * @param run run containing the embedded drawing.
-	 */
-	byte[] getRunDrawingData(R run) {
-		String imageRelId = getPic(run).getBlipFill().getBlip().getEmbed();
-		String imageRelPartName = getImageRelPartName(imageRelId);
-		long size = getImageSize(imageRelPartName);
-		InputStream stream = getImageStream(imageRelPartName);
-		return streamToByteArray(size, stream);
-	}
+    /**
+     * Extract an image bytes from an embedded image run.
+     *
+     * @param run run containing the embedded drawing.
+     */
+    byte[] getRunDrawingData(R run) {
+        String imageRelId = getPic(run).getBlipFill()
+                                       .getBlip()
+                                       .getEmbed();
+        String imageRelPartName = getImageRelPartName(imageRelId);
+        long size = getImageSize(imageRelPartName);
+        InputStream stream = getImageStream(imageRelPartName);
+        return streamToByteArray(size, stream);
+    }
 
-	private static Pic getPic(R run) {
-		for (Object runContent : run.getContent()) {
-			if (!(runContent instanceof JAXBElement<?> runElement)) break;
-			if (!(runElement.getValue() instanceof Drawing drawing)) break;
-			Graphic graphic = getInlineGraphic(drawing);
-			return graphic.getGraphicData().getPic();
-		}
-		throw new OfficeStamperException("Run drawing not found !");
-	}
+    private static Pic getPic(R run) {
+        for (Object runContent : run.getContent()) {
+            if (!(runContent instanceof JAXBElement<?> runElement)) break;
+            if (!(runElement.getValue() instanceof Drawing drawing)) break;
+            Graphic graphic = getInlineGraphic(drawing);
+            return graphic.getGraphicData()
+                          .getPic();
+        }
+        throw new OfficeStamperException("Run drawing not found !");
+    }
 
-	private String getImageRelPartName(String imageRelId) {
-		// TODO: find a better way to find image rel part name in source part store
-		return wordprocessingMLPackage
-				.getMainDocumentPart()
-				.getRelationshipsPart()
-				.getPart(imageRelId)
-				.getPartName()
-				.getName()
-				.substring(1);
-	}
+    private String getImageRelPartName(String imageRelId) {
+        // TODO: find a better way to find image rel part name in source part store
+        return wordprocessingMLPackage.getMainDocumentPart()
+                                      .getRelationshipsPart()
+                                      .getPart(imageRelId)
+                                      .getPartName()
+                                      .getName()
+                                      .substring(1);
+    }
+
+    private long getImageSize(String imageRelPartName) {
+        try {
+            return wordprocessingMLPackage.getSourcePartStore()
+                                          .getPartSize(imageRelPartName);
+        } catch (Docx4JException e) {
+            throw new OfficeStamperException(e);
+        }
+    }
+
+    private InputStream getImageStream(String imageRelPartName) {
+        try {
+            return wordprocessingMLPackage.getSourcePartStore()
+                                          .loadPart(imageRelPartName);
+        } catch (Docx4JException e) {
+            throw new OfficeStamperException(e);
+        }
+    }
+
+    /**
+     * Converts an InputStream to a byte array.
+     *
+     * @param size expected size of the byte array.
+     * @param is   input stream to read data from.
+     *
+     * @return the data from the input stream.
+     */
+    private static byte[] streamToByteArray(long size, InputStream is) {
+        if (size > Integer.MAX_VALUE) throw new OfficeStamperException("Image size exceeds maximum allowed (2GB)");
+
+        int intSize = (int) size;
+        byte[] data = new byte[intSize];
+        int numRead = tryRead(is, data);
+        return Arrays.copyOfRange(data, 0, numRead);
+    }
 
     /**
      * Extract an inline graphic from a drawing.
@@ -80,78 +117,51 @@ public class DocxImageExtractor {
         throw new OfficeStamperException("Don't know how to process anchor !");
     }
 
-	/**
-	 * Converts an InputStream to a byte array.
-	 *
-	 * @param size expected size of the byte array.
-	 * @param is   input stream to read data from.
-	 * @return the data from the input stream.
-	 */
-	private static byte[] streamToByteArray(long size, InputStream is) {
-		if (size > Integer.MAX_VALUE)
-			throw new OfficeStamperException("Image size exceeds maximum allowed (2GB)");
+    private static int tryRead(InputStream is, byte[] data) {
+        try {
+            return is.read(data);
+        } catch (IOException e) {
+            throw new OfficeStamperException(e);
+        }
+    }
 
-		int intSize = (int) size;
-		byte[] data = new byte[intSize];
-		int numRead = tryRead(is, data);
-		return Arrays.copyOfRange(data, 0, numRead);
-	}
+    /**
+     * Extract the name of the image from an embedded image run.
+     *
+     * @param run run containing the embedded drawing.
+     *
+     * @return a {@link String} object
+     */
+    public String getRunDrawingFilename(R run) {
+        return getPic(run).getNvPicPr()
+                          .getCNvPr()
+                          .getName();
+    }
 
-	private static int tryRead(InputStream is, byte[] data) {
-		try {
-			return is.read(data);
-		} catch (IOException e) {
-			throw new OfficeStamperException(e);
-		}
-	}
+    /**
+     * Extract the content type of the image from an embedded image run.
+     *
+     * @param run run containing the embedded drawing.
+     *
+     * @return a {@link String} object
+     */
+    public String getRunDrawingAltText(R run) {
+        return getPic(run).getNvPicPr()
+                          .getCNvPr()
+                          .getDescr();
+    }
 
-	private long getImageSize(String imageRelPartName) {
-		try {
-			return wordprocessingMLPackage
-					.getSourcePartStore()
-					.getPartSize(imageRelPartName);
-		} catch (Docx4JException e) {
-			throw new OfficeStamperException(e);
-		}
-	}
-
-	private InputStream getImageStream(String imageRelPartName) {
-		try {
-			return wordprocessingMLPackage
-					.getSourcePartStore()
-					.loadPart(imageRelPartName);
-		} catch (Docx4JException e) {
-			throw new OfficeStamperException(e);
-		}
-	}
-
-	/**
-	 * Extract the name of the image from an embedded image run.
-	 *
-	 * @param run run containing the embedded drawing.
-	 * @return a {@link String} object
-	 */
-	public String getRunDrawingFilename(R run) {
-		return getPic(run).getNvPicPr().getCNvPr().getName();
-	}
-
-	/**
-	 * Extract the content type of the image from an embedded image run.
-	 *
-	 * @param run run containing the embedded drawing.
-	 * @return a {@link String} object
-	 */
-	public String getRunDrawingAltText(R run) {
-		return getPic(run).getNvPicPr().getCNvPr().getDescr();
-	}
-
-	/**
-	 * Extract the width of the image from an embedded image run.
-	 *
-	 * @param run run containing the embedded drawing.
-	 * @return a {@link Integer} object
-	 */
-	public Integer getRunDrawingMaxWidth(R run) {
-		return (int) getPic(run).getSpPr().getXfrm().getExt().getCx();
-	}
+    /**
+     * Extract the width of the image from an embedded image run.
+     *
+     * @param run run containing the embedded drawing.
+     *
+     * @return a {@link Integer} object
+     */
+    public Integer getRunDrawingMaxWidth(R run) {
+        return (int) getPic(run).getSpPr()
+                                .getXfrm()
+                                .getExt()
+                                .getCx();
+    }
 }
