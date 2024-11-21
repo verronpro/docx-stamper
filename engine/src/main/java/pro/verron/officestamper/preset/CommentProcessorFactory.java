bf5687a48733ed593739e8a0442b9f0470b6eb33
Joseph Verron <joseph@verron.pro>
2024-11-21 21:57:17 +0800

    refactor: change context types from List to Iterable in repeat processors
    Use Iterable instead of List for better flexibility and to accommodate various data structures.

--- engine/src/main/java/pro/verron/officestamper/preset/CommentProcessorFactory.java
+++ engine/src/main/java/pro/verron/officestamper/preset/CommentProcessorFactory.java
@@ -7,7 +7,6 @@ import pro.verron.officestamper.api.OfficeStamper;
 import pro.verron.officestamper.api.OfficeStamperConfiguration;
 import pro.verron.officestamper.api.ParagraphPlaceholderReplacer;
 import pro.verron.officestamper.core.DocxStamper;
-import pro.verron.officestamper.core.PlaceholderReplacer;
 import pro.verron.officestamper.preset.processors.displayif.DisplayIfProcessor;
 import pro.verron.officestamper.preset.processors.repeat.RepeatProcessor;
 import pro.verron.officestamper.preset.processors.repeatdocpart.RepeatDocPartProcessor;
@@ -15,45 +14,35 @@ import pro.verron.officestamper.preset.processors.repeatparagraph.ParagraphRepea
 import pro.verron.officestamper.preset.processors.replacewith.ReplaceWithProcessor;
 import pro.verron.officestamper.preset.processors.table.TableResolver;
 
-import java.util.List;
-
-/**
- * Factory class to create the correct comment processor for a given comment.
- *
- * @author Joseph Verron
- * @version ${version}
- * @since 1.6.4
- */
+/// Factory class to create the correct comment processor for a given comment.
+///
+/// @author Joseph Verron
+/// @version ${version}
+/// @since 1.6.4
 public class CommentProcessorFactory {
     private final OfficeStamperConfiguration configuration;
 
-    /**
-     * Creates a new CommentProcessorFactory.
-     *
-     * @param configuration the configuration to use for the created processors.
-     */
+    /// Creates a new CommentProcessorFactory.
+    ///
+    /// @param configuration the configuration to use for the created processors.
     public CommentProcessorFactory(OfficeStamperConfiguration configuration) {
         this.configuration = configuration;
     }
 
-    /**
-     * Creates a new CommentProcessorFactory with default configuration.
-     *
-     * @param pr a {@link PlaceholderReplacer} object
-     *
-     * @return a {@link CommentProcessor} object
-     */
+    /// Creates a new CommentProcessorFactory with default configuration.
+    ///
+    /// @param pr a [ParagraphPlaceholderReplacer] object
+    ///
+    /// @return a [CommentProcessor] object
     public CommentProcessor repeatParagraph(ParagraphPlaceholderReplacer pr) {
         return ParagraphRepeatProcessor.newInstance(pr);
     }
 
-    /**
-     * Creates a new CommentProcessorFactory with default configuration.
-     *
-     * @param pr a {@link PlaceholderReplacer} object
-     *
-     * @return a {@link CommentProcessor} object
-     */
+    /// Creates a new CommentProcessorFactory with default configuration.
+    ///
+    /// @param pr a [ParagraphPlaceholderReplacer] object
+    ///
+    /// @return a [CommentProcessor] object
     public CommentProcessor repeatDocPart(ParagraphPlaceholderReplacer pr) {
         return RepeatDocPartProcessor.newInstance(pr, getStamper());
     }
@@ -62,205 +51,136 @@ public class CommentProcessorFactory {
         return (template, context, output) -> new DocxStamper(configuration).stamp(template, context, output);
     }
 
-    /**
-     * Creates a new CommentProcessorFactory with default configuration.
-     *
-     * @param pr a {@link PlaceholderReplacer} object
-     *
-     * @return a {@link CommentProcessor} object
-     */
+    /// Creates a new CommentProcessorFactory with default configuration.
+    ///
+    /// @param pr a [ParagraphPlaceholderReplacer] object
+    ///
+    /// @return a [CommentProcessor] object
     public CommentProcessor repeat(ParagraphPlaceholderReplacer pr) {
         return RepeatProcessor.newInstance(pr);
     }
 
-    /**
-     * Creates a new CommentProcessorFactory with default configuration.
-     *
-     * @param pr a {@link PlaceholderReplacer} object
-     *
-     * @return a {@link CommentProcessor} object
-     */
+    /// Creates a new CommentProcessorFactory with default configuration.
+    ///
+    /// @param pr a [ParagraphPlaceholderReplacer] object
+    ///
+    /// @return a [CommentProcessor] object
     public CommentProcessor tableResolver(ParagraphPlaceholderReplacer pr) {
         return TableResolver.newInstance(pr);
     }
 
-    /**
-     * Creates a new CommentProcessorFactory with default configuration.
-     *
-     * @param pr a {@link PlaceholderReplacer} object
-     *
-     * @return a {@link CommentProcessor} object
-     */
+    /// Creates a new CommentProcessorFactory with default configuration.
+    ///
+    /// @param pr a [ParagraphPlaceholderReplacer] object
+    ///
+    /// @return a [CommentProcessor] object
     public CommentProcessor displayIf(ParagraphPlaceholderReplacer pr) {
         return DisplayIfProcessor.newInstance(pr);
     }
 
-    /**
-     * Creates a new CommentProcessorFactory with default configuration.
-     *
-     * @param pr a {@link PlaceholderReplacer} object
-     *
-     * @return a {@link CommentProcessor} object
-     */
+    /// Creates a new CommentProcessorFactory with default configuration.
+    ///
+    /// @param pr a [ParagraphPlaceholderReplacer] object
+    ///
+    /// @return a [CommentProcessor] object
     public CommentProcessor replaceWith(ParagraphPlaceholderReplacer pr) {
         return ReplaceWithProcessor.newInstance(pr);
     }
 
-    /**
-     * This interface is used to resolve a table in the template document.
-     * The table is passed to the resolveTable method and will be used to fill an existing Tbl object in the document.
-     *
-     * @author Joseph Verron
-     * @version ${version}
-     * @since 1.6.2
-     */
+    /// This interface is used to resolve a table in the template document.
+    /// The table is passed to the resolveTable method and will be used to fill an existing Tbl object in the document.
+    ///
+    /// @author Joseph Verron
+    /// @version ${version}
+    /// @since 1.6.2
     public interface ITableResolver {
-        /**
-         * Resolves the given table by manipulating the given table in the template
-         *
-         * @param table the table to resolve.
-         */
+        /// Resolves the given table by manipulating the given table in the template
+        ///
+        /// @param table the table to resolve.
         void resolveTable(@Nullable StampTable table);
     }
 
-    /**
-     * Interface for processors that replace a single word with an expression defined
-     * in a comment.
-     *
-     * @author Joseph Verron
-     * @author Tom Hombergs
-     * @version ${version}
-     * @since 1.0.8
-     */
+    /// Interface for processors that replace a single word with an expression defined
+    /// in a comment.
+    ///
+    /// @author Joseph Verron
+    /// @author Tom Hombergs
+    /// @version ${version}
+    /// @since 1.0.8
     public interface IReplaceWithProcessor {
 
-        /**
-         * May be called to replace a single word inside a paragraph with an expression
-         * defined in a comment. The comment must be applied to a single word for the
-         * replacement to take effect!
-         *
-         * @param expression the expression to replace the text with
-         */
+        /// May be called to replace a single word inside a paragraph with an expression
+        /// defined in a comment. The comment must be applied to a single word for the
+        /// replacement to take effect!
+        ///
+        /// @param expression the expression to replace the text with
         void replaceWordWith(@Nullable String expression);
     }
 
-    /**
-     * Implementations of this interface are responsible for processing the repeat paragraph instruction.
-     * The repeat paragraph instruction is a comment that contains the following text:
-     * <p>
-     * <code>
-     * repeatParagraph(...)
-     * </code>
-     * <p>
-     * Where the three dots represent an expression that evaluates to a list of objects.
-     * The processor then copies the paragraph once for each object in the list and evaluates all expressions
-     * within each copy against the respective object.
-     *
-     * @author Joseph Verron
-     * @author Romain Lamarche
-     * @version ${version}
-     * @since 1.0.0
-     */
+    /// An interface that defines a processor for repeating a paragraph
+    /// for each element present in the given iterable collection of objects.
+    ///
+    /// @author Joseph Verron
+    /// @author Romain Lamarche
+    /// @version ${version}
+    /// @since 1.0.0
     public interface IParagraphRepeatProcessor {
-
-        /**
-         * May be called to mark a paragraph to be copied once for each element in the passed-in list.
-         * Within each copy of the row, all expressions are evaluated against one of the objects in the list.
-         *
-         * @param objects the objects which serve as context root for expressions found in the template table row.
-         */
-        void repeatParagraph(@Nullable List<Object> objects);
+        /// Mark a paragraph to be copied once for each element in the passed-in iterable.
+        /// Within each copy, placeholder evaluation context is the next object in the iterable.
+        ///
+        /// @param objects objects serving as evaluation context seeding a new copy.
+        void repeatParagraph(@Nullable Iterable<Object> objects);
     }
 
-    /**
-     * Interface for processors which may be called to mark a document part to be copied once for each element in the
-     * passed-in list.
-     * Within each copy of the row, all expressions are evaluated against one of the objects in the list.
-     *
-     * @author Joseph Verron
-     * @author Artem Medvedev
-     * @version ${version}
-     * @since 1.0.0
-     */
+    /// An interface that defines a processor for repeating a document part
+    /// for each element present in the given iterable collection of objects.
+    ///
+    /// @author Joseph Verron
+    /// @author Artem Medvedev
+    /// @version ${version}
+    /// @since 1.0.0
     public interface IRepeatDocPartProcessor {
-
-        /**
-         * May be called to mark a document part to be copied once for each element in the passed-in list.
-         * Within each copy of the row, all expressions are evaluated against one of the objects in the list.
-         *
-         * @param objects the objects which serve as context root for expressions found in the template table row.
-         */
-        void repeatDocPart(@Nullable List<Object> objects);
+        /// Mark a document part to be copied once for each element in the passed-in iterable.
+        /// Within each copy, placeholder evaluation context is the next object in the iterable.
+        ///
+        /// @param objects objects serving as evaluation context seeding a new copy.
+        void repeatDocPart(@Nullable Iterable<Object> objects);
     }
 
-    /**
-     * Interface for processors that can repeat a table row.
-     *
-     * @author Joseph Verron
-     * @author Tom Hombergs
-     * @version ${version}
-     * @since 1.0.0
-     */
+    /// An interface that defines a processor for repeating a table row
+    /// for each element present in the given iterable collection of objects.
+    ///
+    /// @author Joseph Verron
+    /// @author Tom Hombergs
+    /// @version ${version}
+    /// @since 1.0.0
     public interface IRepeatProcessor {
-
-        /**
-         * May be called to mark a table row to be copied once for each element in the passed-in list.
-         * Within each copy of the row, all expressions are evaluated against one of the objects in the list.
-         *
-         * @param objects the objects which serve as context root for expressions found in the template table row.
-         */
-        void repeatTableRow(@Nullable List<Object> objects);
-
+        /// Mark a table row to be copied once for each element in the passed-in iterable.
+        /// Within each copy, placeholder evaluation context is the next object in the iterable.
+        ///
+        /// @param objects objects serving as evaluation context seeding a new copy.
+        void repeatTableRow(@Nullable Iterable<Object> objects);
     }
 
-    /**
-     * Interface for processors that may be used to delete commented paragraphs or tables from the document, depending
-     * on a given condition.
-     *
-     * @author Joseph Verron
-     * @author Tom Hombergs
-     * @version ${version}
-     * @since 1.0.0
-     */
+    /// Interface for processors that may be used to delete commented paragraphs or tables from the document, depending
+    /// on a given condition.
+    ///
+    /// @author Joseph Verron
+    /// @author Tom Hombergs
+    /// @version ${version}
+    /// @since 1.0.0
     public interface IDisplayIfProcessor {
 
-        /**
-         * May be called to delete the commented paragraph or not, depending on the given boolean condition.
-         *
-         * @param condition if true, the commented paragraph will remain in the document. If false, the commented
-         *                  paragraph
-         *                  will be deleted at stamping.
-         */
+        /// @param condition if true, the paragraph surrounding the comment remains, else is removed.
         void displayParagraphIf(@Nullable Boolean condition);
 
-        /**
-         * May be called to delete the commented paragraph or not, depending on the presence of the given data.
-         *
-         * @param condition if non-null, the commented paragraph will remain in
-         *                  the document. If null, the commented paragraph
-         *                  will be deleted at stamping.
-         */
+        /// @param condition if non-null, the paragraph surrounding the comment remains, else is removed.
         void displayParagraphIfPresent(@Nullable Object condition);
 
-        /**
-         * May be called to delete the table surrounding the commented paragraph, depending on the given boolean
-         * condition.
-         *
-         * @param condition if true, the table row surrounding the commented paragraph will remain in the document. If
-         *                  false, the table row
-         *                  will be deleted at stamping.
-         */
+        /// @param condition if true, the table row surrounding the comment remains, else is removed.
         void displayTableRowIf(@Nullable Boolean condition);
 
-        /**
-         * May be called to delete the table surrounding the commented paragraph, depending on the given boolean
-         * condition.
-         *
-         * @param condition if true, the table surrounding the commented paragraph will remain in the document. If
-         *                  false, the table
-         *                  will be deleted at stamping.
-         */
+        /// @param condition if true, the table surrounding the comment remains, else is removed.
         void displayTableIf(@Nullable Boolean condition);
     }
-
 }
--- engine/src/main/java/pro/verron/officestamper/preset/processors/repeatparagraph/ParagraphRepeatProcessor.java
+++ engine/src/main/java/pro/verron/officestamper/preset/processors/repeatparagraph/ParagraphRepeatProcessor.java
@@ -10,63 +10,54 @@ import pro.verron.officestamper.core.StandardParagraph;
 import pro.verron.officestamper.preset.CommentProcessorFactory;
 import pro.verron.officestamper.preset.Paragraphs;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
+import static java.util.Collections.emptyIterator;
 import static pro.verron.officestamper.core.SectionUtil.getPreviousSectionBreakIfPresent;
 import static pro.verron.officestamper.core.SectionUtil.hasOddNumberOfSectionBreaks;
 
-/**
- * This class is used to repeat paragraphs and tables.
- * <p>
- * It is used internally by the DocxStamper and should not be instantiated by
- * clients.
- *
- * @author Joseph Verron
- * @author Youssouf Naciri
- * @version ${version}
- * @since 1.2.2
- */
+/// Class used internally to repeat document elements.
+/// Used by the lib, should not be instantiated by clients.
+///
+/// @author Joseph Verron
+/// @author Youssouf Naciri
+/// @version ${version}
+/// @since 1.2.2
 public class ParagraphRepeatProcessor
         extends AbstractCommentProcessor
         implements CommentProcessorFactory.IParagraphRepeatProcessor {
-    private Map<Paragraph, Paragraphs> pToRepeat = new HashMap<>(); // TODO replace the mapping by a Paragraphs to
-    // List<Object> mapping to better reflect the change
 
-    /**
-     * @param placeholderReplacer replaces placeholders with values
-     */
+    /* TODO replace the mapping by a Paragraphs to List<Object> mapping to better reflect the change*/
+    private Map<Paragraph, Paragraphs> pToRepeat = new HashMap<>();
+
     private ParagraphRepeatProcessor(ParagraphPlaceholderReplacer placeholderReplacer) {
         super(placeholderReplacer);
     }
 
     /**
-     * <p>newInstance.</p>
+     * Creates a new instance of {@link CommentProcessor} using the provided {@link ParagraphPlaceholderReplacer}.
      *
-     * @param placeholderReplacer replaces expressions with values
-     *
-     * @return a new instance of ParagraphRepeatProcessor
+     * @param placeholderReplacer the replacer to use for processing paragraph placeholders.
+     * @return a new instance of {@link ParagraphRepeatProcessor}.
      */
     public static CommentProcessor newInstance(ParagraphPlaceholderReplacer placeholderReplacer) {
         return new ParagraphRepeatProcessor(placeholderReplacer);
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override public void repeatParagraph(List<Object> objects) {
-        var paragraph = this.getParagraph();
+    @Override public void repeatParagraph(Iterable<Object> objects) {
+        var paragraph = getParagraph();
         var comment = getCurrentCommentWrapper();
-        var data = new ArrayDeque<>(objects);
         var elements = comment.getElements();
         var previousSectionBreak = getPreviousSectionBreakIfPresent(elements.getFirst(), comment.getParent());
         var oddNumberOfBreaks = hasOddNumberOfSectionBreaks(elements);
-        var toRepeat = new Paragraphs(comment, data, elements, previousSectionBreak, oddNumberOfBreaks);
+        var iterator = objects == null ? emptyIterator() : objects.iterator();
+        var toRepeat = new Paragraphs(comment, iterator, elements, previousSectionBreak, oddNumberOfBreaks);
         pToRepeat.put(paragraph, toRepeat);
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override public void commitChanges(DocxPart document) {
         for (Map.Entry<Paragraph, Paragraphs> entry : pToRepeat.entrySet()) {
             var current = entry.getKey();
@@ -77,14 +68,10 @@ public class ParagraphRepeatProcessor
         }
     }
 
-    private List<P> generateParagraphsToAdd(
-            DocxPart document,
-            Paragraphs paragraphs
-    ) {
+    private List<P> generateParagraphsToAdd(DocxPart document, Paragraphs paragraphs) {
         var paragraphsToAdd = new LinkedList<P>();
-        var last = paragraphs.data()
-                             .peekLast();
-        for (Object expressionContext : paragraphs.data()) {
+        for (var it = paragraphs.data(); it.hasNext(); ) {
+            Object expressionContext = it.next();
             for (Object paragraphToClone : paragraphs.elements()) {
                 Object clone = XmlUtils.deepCopy(paragraphToClone);
                 var comment = paragraphs.comment();
@@ -100,18 +87,14 @@ public class ParagraphRepeatProcessor
                 }
             }
             var sectPr = paragraphs.previousSectionBreak();
-            if (paragraphs.oddNumberOfBreaks() && sectPr.isPresent() && expressionContext != last) {
+            if (paragraphs.oddNumberOfBreaks() && sectPr.isPresent() && it.hasNext()) {
                 assert paragraphsToAdd.peekLast() != null : "There should be at least one ";
-                SectionUtil.applySectionBreakToParagraph(sectPr.get(),
-                        paragraphsToAdd.peekLast());
+                SectionUtil.applySectionBreakToParagraph(sectPr.get(), paragraphsToAdd.peekLast());
             }
         }
         return paragraphsToAdd;
     }
 
-    /**
-     * {@inheritDoc}
-     */
     @Override public void reset() {
         pToRepeat = new HashMap<>();
     }
